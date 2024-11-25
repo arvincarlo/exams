de8812579dce589c87c0c9fd6de2b6b3b72f8329
Arvin <arvincris@gmail.com>
2024-11-25 11:49:30 +0800

    Creating a useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -3,70 +3,91 @@ import './App.css'
 
 const initialState = {
   query: "",
-  notFound: false
+  notFound: false,
+  windspeed: "",
+  temperature: "",
+  humidity: "",
+  cities: []
 }
 
 const WeatherData = {
-  "Makati": {
-    windSpeed: 10,
-    temperature: 20,
-    humidity: 50,
+  "Manila": {
+    windspeed: "12 km/h",
+    temperature: "32°C",
+    humidity: "75%"
   },
-  "BGC": {
-    windSpeed: 20,
-    temperature: 20,
-    humidity: 50,
+  "Cebu City": {
+    windspeed: "10 km/h",
+    temperature: "30°C",
+    humidity: "80%"
   },
-  "Mandaluyong": {
-    windSpeed: 30,
-    temperature: 20,
-    humidity: 50,
+  "Davao City": {
+    windspeed: "8 km/h",
+    temperature: "31°C",
+    humidity: "78%"
   },
+  "Baguio": {
+    windspeed: "15 km/h",
+    temperature: "20°C",
+    humidity: "85%"
+  }
 }
 
 const CITIES = Object.keys(WeatherData);
 
 function reducer(state, action) {
-  switch(action.type) {
-    case "setQuery":
+  switch (action.type) {
+    case "setQuery": 
       return {
         ...state,
         query: action.payload
       }
-
+    
     case "search": 
-      if (!state.query) return state;
-      if (!CITIES.includes(state.query)) {
+      if (!action.payload) return state;
+      if (!CITIES.includes(action.payload)) {
         return {
           ...state,
           notFound: true
         }
       }
-    break;
 
-    default: 
-      return state
+      return {
+        query: action.payload,
+        temperature: WeatherData[action.payload].temperature,
+        windspeed: WeatherData[action.payload].windspeed,
+        humidity: WeatherData[action.payload].humidity,
+        notFound: false,
+        cities: (state.cities.includes(action.payload)) ? state.cities : [...state.cities, action.payload]
+      }
+    default: return state;
   }
 }
 
 function App() {
-  const [{query, notFound}, dispatch] = useReducer(reducer, initialState);
+
+  const [{query, temperature, windspeed, humidity, notFound, cities}, dispatch] = useReducer(reducer, initialState);
+  console.log(cities);
+  
 
   return (
     <>
       <div>
         <input type="text" value={query} onChange={(e) => dispatch({type: "setQuery", payload: e.target.value})}/>
-        <button onClick={() => dispatch({type: "search"})}>Search</button>
+        <button onClick={() => dispatch({type: "search", payload: query})}>Search</button>
+        <ul>
+          {cities?.map(city => <button onClick={() => dispatch({type: "search", payload: city})} key={city}>{city}</button>)}
+        </ul>
       </div> 
       <div>
         <div>
-          Temperature: 
+          Temperature: {!notFound && temperature}
         </div>  
         <div>
-          Windspeed: 
+          Windspeed: {!notFound && windspeed}
         </div>  
         <div>
-          Humidity: 
+          Humidity: {!notFound && humidity}
         </div>  
         {notFound && <div>City not found</div>}
       </div> 
